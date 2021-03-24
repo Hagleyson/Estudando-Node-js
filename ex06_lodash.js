f1e8701a784540949adc5c09ee5eaff6556bd283
hagleyson <hagleyson@gmail.com>
2021-03-24 03:01:24 -0300

    aula 6

---
+++ ex06_lodash.js
@@ -0,0 +1,20 @@
+const _ = require("lodash");
+
+const alunos = [
+  {
+    nome: "hagleyson",
+    nota: 10,
+  },
+  {
+    nome: "teste",
+    nota: 6,
+  },
+  {
+    nome: "teste1",
+    nota: 8,
+  },
+];
+
+const media = _.sumBy(alunos, "nota") / alunos.length;
+console.log(media);
+//instalar lodash npm i lodash
